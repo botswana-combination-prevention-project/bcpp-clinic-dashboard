b29b20a0dc838fb96c98d1f4f905298310be2849
erikvw <ew2789@gmail.com>
2017-08-07 16:07:43 +0200

    initial

---
+++ bcpp_clinic_dashboard/model_wrappers/subject_visit_model_wrapper.py
@@ -0,0 +1,14 @@
+from django.apps import apps as django_apps
+from edc_model_wrapper import ModelWrapper
+
+
+class SubjectVisitModelWrapper(ModelWrapper):
+
+    model = 'bcpp_clinic_subject.subjectvisit'
+    next_url_name = django_apps.get_app_config(
+        'bcpp_clinic_subject').dashboard_url_name
+    next_url_attrs = ['subject_identifier', 'appointment']
+
+    @property
+    def appointment(self):
+        return self.object.appointment.id
---
+++ bcpp_clinic_dashboard/views/dashboard/default/dashboard_view.py
@@ -0,0 +1,55 @@
+from django.apps import apps as django_apps
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.decorators import method_decorator
+from django.views.generic.base import TemplateView
+
+from edc_base.view_mixins import EdcBaseViewMixin
+from edc_dashboard.view_mixins import (
+    DashboardViewMixin as EdcDashboardViewMixin, AppConfigViewMixin)
+
+from ....model_wrappers import CrfModelWrapper, SubjectVisitModelWrapper
+from ....model_wrappers import RequisitionModelWrapper, SubjectConsentModelWrapper
+from .base_dashboard_view import BaseDashboardView
+
+
+class DashboardView(
+        BaseDashboardView, EdcDashboardViewMixin,
+        AppConfigViewMixin, EdcBaseViewMixin, TemplateView):
+    app_config_name = 'bcpp_clinic_subject'
+    navbar_item_selected = 'bcpp_clinic_subject'
+    consent_model_wrapper_class = SubjectConsentModelWrapper
+    off_study_model = 'bcpp_clinic_subject.subjectoffstudy'
+    crf_model_wrapper_cls = CrfModelWrapper
+    requisition_model_wrapper_cls = RequisitionModelWrapper
+    visit_model_wrapper_cls = SubjectVisitModelWrapper
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.anonymous = None
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    @property
+    def subject_off_study_model_cls(self):
+        return django_apps.get_model(self.off_study_model)
+
+    @property
+    def subject_consent_model_cls(self):
+        return django_apps.get_model(self.consent_model)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        dashboard_url_name = django_apps.get_app_config(
+            'bcpp_clinic_subject').dashboard_url_name,
+        try:
+            subject_offstudy = self.subject_off_study_model_cls.objects.get(
+                subject_identifier=self.subject_identifier)
+        except ObjectDoesNotExist:
+            subject_offstudy = None
+        context.update(
+            subject_offstudy=subject_offstudy,
+            bcpp_clinic_subject_dashboard_url_name=dashboard_url_name)
+        return context
