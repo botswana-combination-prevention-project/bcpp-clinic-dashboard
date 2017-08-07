c20a629f36c478255f484be35c3f5942a50e52c4
erikvw <ew2789@gmail.com>
2017-08-07 22:35:57 +0200

    templates, apps

--- bcpp_clinic_dashboard/apps.py
+++ bcpp_clinic_dashboard/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig as DjangoApponfig
 
 class AppConfig(DjangoApponfig):
     name = 'bcpp_clinic_dashboard'
+    listboard_template_name = 'bcpp_clinic_dashboard/listboard.html'
+    dashboard_template_name = 'bcpp_clinic_dashboard/dashboard.html'
+    base_template_name = 'bcpp_clinic/base.html'
+    listboard_url_name = 'bcpp_clinic_dashboard:listboard_url'
+    dashboard_url_name = 'bcpp_clinic_dashboard:dashboard_url'
